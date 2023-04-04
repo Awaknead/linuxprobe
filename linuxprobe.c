8aea6f79f5dfccfa5423ed1f7d80c98b46a6f20c
Awaknead <m13636311242@163.com>
2023-04-04 20:48:49 +0800

    linux-01

---
+++ linuxprobe.c
@@ -0,0 +1,13 @@
+//linux命令
+// 带   []     的是可以省略也可以加上的
+#include<unistd.h>
+#include<sys/types.h>
+#include<errno.h>
+#include<math.h>
+// git@github.com:Awaknead/myproject1.git
+echo;//<显示字符串> <echo 字符串>
+date;//<显示/修改 系统时间> <date [+][参数]>
+timedatectl;//<显示/修改 时区，系统时间> <timedatectl 参数>
+reboot;//<重启系统>
+poweroff;//<关闭系统>
+wget;//<下载网络文件><wad get>  <wget [参数]网址>
